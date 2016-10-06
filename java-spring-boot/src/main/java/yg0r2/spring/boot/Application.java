cc43b527b8c5967ba309413ce2f1859a3ba70d95
Yg0R2 <yg0r2@yahoo.com>
2016-10-06 15:24:44 +0200

    Add basic Spring Boot stuff

---
+++ java-spring-boot/src/main/java/yg0r2/spring/boot/Application.java
@@ -0,0 +1,12 @@
+package yg0r2.spring.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
